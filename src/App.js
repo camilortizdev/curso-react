1fe8720e687ffb4dabd9dbfc31ba33c4b6c1aa02
kmilo <camiloweb9@gmail.com>
2023-02-04 20:45:44 -0500

    Se agregaron los TODOs con sus estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from "react";
+import { TodoCounter } from "./TodoCounter"
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+// import "./App.css";
 
 const todos = [
-  { Text: "Cortar cebolla", completed: false},
-  { Text: "Tomar el curso de intro con react", completed: false},
+  { text: "Cortar cebolla", completed: true},
+  { text: "Tomar el curso de intro con react", completed: true},
   { text: "Llorar", completed: false},
 ]
 
 function App() {
   return (
-    <>
-    <TodoCounter />
-    <h2>Has completado 2 de 3 TODOs</h2>
-    <TodoSearch />
-    <input placeholder="Cebolla"></input>
-    <TodoList>
-      {todos.map(todo=>(
-        <TodoItem />
-      ))}
-    </TodoList>
-    <CreateTodoButton />
-    <button>+</button>
-    </>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo=> (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
